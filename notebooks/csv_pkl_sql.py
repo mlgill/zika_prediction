af0fec648cef94a0bd8735a03ffad98833d09acf
Michelle Gill <github@michellelynngill.com>
2016-07-24 16:10:48 -0400

    Add my csv/pkl/sql maker

---
+++ notebooks/csv_pkl_sql.py
@@ -0,0 +1,26 @@
+from api_keys import POSTGRES_PWD
+import dill
+import pandas as pd
+from sqlalchemy import create_engine
+
+def csv_it(dataframe, filebase):
+    dataframe.to_csv('../csv/'+filebase+'.csv', sep=',', index=False, encoding='utf-8')
+    return
+
+def pkl_it(dataframe, filebase):
+    with open('../pkl/'+filebase+'.pkl', 'w') as fh:
+        dill.dump(dataframe, fh)
+
+    return
+
+def sql_it(dataframe, filebase, dbname='mcnulty'):
+    postgres_str = 'postgresql://mlgill:{}@127.0.0.1:12345/{}'.format(POSTGRES_PWD, dbname)
+    engine = create_engine(postgres_str)
+    dataframe.to_sql(filebase, engine, if_exists='replace')
+    return
+
+def save_it(dataframe, filebase, dbname='mcnulty'):
+    csv_it(dataframe, filebase)
+    pkl_it(dataframe, filebase)
+    sql_it(dataframe, filebase, dbname=dbname)
+    return
