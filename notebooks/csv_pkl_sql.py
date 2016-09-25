918b13d05f2761003c8f9bdbd664660a144c4a5f
Michelle Gill <michelle@michellelynngill.com>
2016-09-25 01:43:33 +0000

    Attempt to fix column formats in SQL

--- notebooks/csv_pkl_sql.py
+++ notebooks/csv_pkl_sql.py
@@ -2,6 +2,7 @@ from api_keys import POSTGRES_PWD
 import dill
 import pandas as pd
 from sqlalchemy import create_engine
+from sqlalchemy.types import NVARCHAR
 
 def csv_it(dataframe, filebase):
     dataframe.to_csv('../csv/'+filebase+'.csv', sep=',', index=False, encoding='utf-8')
@@ -16,7 +17,7 @@ def pkl_it(dataframe, filebase):
 def sql_it(dataframe, filebase, dbname='zika'):
     postgres_str = 'postgresql://mlgill:{}@127.0.0.1:5432/{}'.format(POSTGRES_PWD, dbname)
     engine = create_engine(postgres_str)
-    dataframe.to_sql(filebase, engine, if_exists='replace')
+    dataframe.to_sql(filebase, engine, if_exists='replace', dtype={col_name: NVARCHAR for col_name in filebase})
     return
 
 def save_it(dataframe, filebase, dbname='zika'):
