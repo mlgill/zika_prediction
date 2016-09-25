9a1b1e68e1d3e78f09222c8940128736368ba20c
Michelle Gill <github@michellelynngill.com>
2016-09-25 14:14:06 -0400

    More rsync tinkering

--- rsync.sh
+++ rsync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 src=.
-dest=vultr:/home/ubuntu/zika_prediction
+dest=vultr:/home/mlgill/zika_prediction
 
 rsync --recursive --update --delete --verbose --append \
 --exclude .ipynb_checkpoints --exclude .DS_Store --dry-run \
