0a43650ed2a69c900de091be5ecd94f6810b4516
Michelle Gill <github@michellelynngill.com>
2016-09-25 14:11:50 -0400

    Update rsync

--- rsync.sh
+++ rsync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 src=.
-dest=vultr:/home/ubuntu/project_mcnulty
+dest=vultr:/home/ubuntu/zika_prediction
 
 rsync --recursive --update --delete --verbose --append \
 --exclude .ipynb_checkpoints --exclude .DS_Store --dry-run \
